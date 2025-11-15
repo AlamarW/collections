69662f128c1345cbb3ae2fcd18b20df71a026368
Austin <austinlamarwiggins@gmail.com>
2025-11-14 20:37:02 -0500

    updating models to... actually make sense

--- collections_app/models.py
+++ collections_app/models.py
@@ -1,31 +1,31 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class Collection(models.Model):
     name = models.CharField(max_length=255)
-    owner = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    collection_items = models.ManyToManyField(
+        "Item", related_name="collections", blank=True
     )
+    collection_schema = models.JSONField(default=dict, blank=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        ordering = ['name']
+        ordering = ["name"]
+
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
     item_data = models.JSONField(default=dict, blank=True)
-    collection = models.ForeignKey(
-        Collection,
-        on_delete=models.CASCADE,
-        related_name='items'
+    item_collection = models.ForeignKey(
+        Collection, on_delete=models.CASCADE, related_name="items"
     )
 
     def __str__(self):
         return f"{self.name} ({self.collection.name})"
 
     class Meta:
-        ordering = ['name']
+        ordering = ["name"]
