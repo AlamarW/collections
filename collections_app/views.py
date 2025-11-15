ea72429f44e34a9b68aa5ba67fd8391c415a6b74
Austin <austinlamarwiggins@gmail.com>
2025-11-14 19:10:29 -0500

    just some linting

--- collections_app/views.py
+++ collections_app/views.py
@@ -1,7 +1,8 @@
-from django.http import JsonResponse, HttpResponseNotFound
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_object_or_404
 import json
+
+from django.http import HttpResponseNotFound, JsonResponse
+from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Collection
 
@@ -10,37 +11,35 @@ from .models import Collection
 def collection_list(request):
     # get collections where user == request
     if not request.user.is_authenticated:
-        return JsonResponse({'error': 'Authentication required'}, status=401)
-    if request.method == 'GET':
+        return JsonResponse({"error": "Authentication required"}, status=401)
+    if request.method == "GET":
         collections = Collection.objects.filter(owner=request.user)
         collections_data = [
             {
-                'id': collection.id,
-                'name': collection.name,
+                "id": collection.id,
+                "name": collection.name,
             }
             for collection in collections
         ]
         return JsonResponse(collections_data, safe=False)
-    elif request.method == 'POST':
+    elif request.method == "POST":
         data = json.loads(request.body)
-        collection = Collection.objects.create(
-            name=data['name'],
-            owner=request.user
-        )
-        return JsonResponse({'id': collection.id, 'name': collection.name}, status=201)
+        collection = Collection.objects.create(name=data["name"], owner=request.user)
+        return JsonResponse({"id": collection.id, "name": collection.name}, status=201)
     else:
-        return JsonResponse({'error': 'Method not allowed'}, status=405)
+        return JsonResponse({"error": "Method not allowed"}, status=405)
+
 
 @csrf_exempt
 def collection_detail(request, pk):
     if not request.user.is_authenticated:
-        return JsonResponse({'error': 'Authentication required'}, status=401)
+        return JsonResponse({"error": "Authentication required"}, status=401)
 
-    if request.method == 'GET':
+    if request.method == "GET":
         collection = get_object_or_404(Collection, pk=pk, owner=request.user)
         collection_data = {
-                'id': collection.id,
-                'name': collection.name,
+            "id": collection.id,
+            "name": collection.name,
         }
 
         return JsonResponse(collection_data)
