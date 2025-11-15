9d8e1537d187e72c96286640aebe1fc3ea054e74
Austin <austinlamarwiggins@gmail.com>
2025-11-14 19:59:36 -0500

    path routing changes and full crud for basic functionality

--- collections_app/urls.py
+++ collections_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('api/collection', views.collection_list, name='collection_list'),
-    path('api/collection/<int:pk>', views.collection_detail, name='collection_detail'),
+    path("api/collections/", views.collection_list, name="collection_list"),
+    path("api/collection/<int:pk>", views.collection_detail, name="collection_detail"),
 ]
--- collections_app/views.py
+++ collections_app/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.http import HttpResponseNotFound, JsonResponse
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 
@@ -9,9 +9,14 @@ from .models import Collection
 
 @csrf_exempt
 def collection_list(request):
-    # get collections where user == request
+    """
+    Methods for interacting with a list of collections
+    So far the only Metho that makes sense at this level is GET
+    """
+
     if not request.user.is_authenticated:
         return JsonResponse({"error": "Authentication required"}, status=401)
+    # get collections where user == request
     if request.method == "GET":
         collections = Collection.objects.filter(owner=request.user)
         collections_data = [
@@ -22,24 +27,44 @@ def collection_list(request):
             for collection in collections
         ]
         return JsonResponse(collections_data, safe=False)
-    elif request.method == "POST":
-        data = json.loads(request.body)
-        collection = Collection.objects.create(name=data["name"], owner=request.user)
-        return JsonResponse({"id": collection.id, "name": collection.name}, status=201)
     else:
         return JsonResponse({"error": "Method not allowed"}, status=405)
 
 
 @csrf_exempt
 def collection_detail(request, pk):
+    """
+    Create, Read, Update or Delete a single collection.
+    """
     if not request.user.is_authenticated:
         return JsonResponse({"error": "Authentication required"}, status=401)
 
+    # Create a collection for the user
+    elif request.method == "POST":
+        data = json.loads(request.body)
+        collection = Collection.objects.create(name=data["name"], owner=request.user)
+        return JsonResponse({"id": collection.id, "name": collection.name}, status=201)
+
+    # Read a collection for the user
     if request.method == "GET":
         collection = get_object_or_404(Collection, pk=pk, owner=request.user)
         collection_data = {
             "id": collection.id,
             "name": collection.name,
         }
-
         return JsonResponse(collection_data)
+
+    # Update a collection for the user
+    elif request.method == "PUT":
+        data = json.loads(request.body)
+        collection = get_object_or_404(Collection, pk=pk, owner=request.user)
+        collection.name = data["name"]
+        collection.save()
+        return JsonResponse({"id": collection.id, "name": collection.name}, status=200)
+
+    # Delete collection from the user's collection list
+    elif request.method == "DELETE":
+        data = json.loads(request.body)
+        collection = get_object_or_404(Collection, pk=data["id"], owner=request.user)
+        collection.delete()
+        return JsonResponse({"message": "Collection deleted"}, status=200)
